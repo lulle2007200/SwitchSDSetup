206ebf36e2d67debc38531e680c7a803c0db1971
fake Name <fake@name.com>
2020-02-02 18:09:47 +0100

    updated license information

--- setup.sh
+++ setup.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+#Project: SwitchSDSetup
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#This script was written by github.com/lulle2007200. Make sure that i get credit, if you reuse, redistribute or modify it.
+#I take no responsibility if this script causes damage to your
+#device or dataloss (which it should not).
+
 declare Size
 
 declare -a Partitions
